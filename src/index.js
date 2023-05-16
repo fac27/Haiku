1b7d9c9349ee85fa0b65ba715576bb7a43da1422
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2023-05-16 12:23:10 +0200

    Update index.js
    updated so it matches the flyh-deploy branch I've been working on

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const server = require("./server.js");
-const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`I'm listening to port ${PORT}`));
+const port = process.env.PORT || "8080";
+server.listen(port, () => console.log(`Listening at http://localhost:${port}`));
